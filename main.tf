2c79948c484814cd058f0eceba0578aab556b597
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2024-09-14 11:06:41 +0200

    update

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
+# Define the S3 bucket
 resource "aws_s3_bucket" "weather_bucket" {
-    bucket = "weather_app"
+  bucket = "weather-app-bucket"  # Change the name to something unique
 }
 
-
+# Define the public access block configuration for the bucket
 resource "aws_s3_bucket_public_access_block" "weather_bucket" {
-  bucket = aws_s3_bucket.example.id
+  bucket = aws_s3_bucket.weather_bucket.id
 
   block_public_acls       = true
   block_public_policy     = false
@@ -12,22 +13,22 @@ resource "aws_s3_bucket_public_access_block" "weather_bucket" {
   restrict_public_buckets = false
 }
 
-# Create an IAM policy document that allows public read access to the S3 bucket
+# Create an IAM policy document for the S3 bucket
 data "aws_iam_policy_document" "weather_bucket_policy" {
   statement {
-    sid    = "AllowPublic"  # Statement ID for identification
-    effect = "Allow"       # Allow the specified actions
-    actions = ["s3:GetObject"]  # Allow read access to objects in the bucket
-    resources = ["${aws_s3_bucket.bucket.arn}/*"]  # Apply policy to all objects in the bucket
+    sid    = "AllowPublicRead"
+    effect = "Allow"
+    actions = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.weather_bucket.arn}/*"]
     principals {
-      type = "*"  # Apply to any principal (user or service)
-      identifiers = ["*"]  # Allow access to everyone
+      type        = "*"
+      identifiers = ["*"]
     }
   }
 }
 
-# Attach the policy to the S3 bucket
+# Attach the IAM policy to the S3 bucket
 resource "aws_s3_bucket_policy" "weather_bucket_policy" {
-  bucket = aws_s3_bucket.bucket.id  # Reference to the S3 bucket created above
-  policy = data.aws_iam_policy_document.weather_bucket_policy  # JSON representation of the IAM policy document
+  bucket = aws_s3_bucket.weather_bucket.id
+  policy = data.aws_iam_policy_document.weather_bucket_policy.json
 }
--- providers.tf
+++ providers.tf
@@ -1,16 +1,24 @@
+# Specify the minimum version of Terraform required for this configuration
 terraform {
+  required_version = "1.9.5"  # Terraform version 1.9.5 or higher is required
+
+  # Define the required providers and their versions
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+      source  = "hashicorp/aws"  # Provider source from HashiCorp
+      version = "5.66.0"         # Exact version of the AWS provider required
+    }
+    archive = {
+      source = "hashicorp/archive"
+      version = "2.6.0"
     }
   }
-}
-# Configure the AWS Provider
+
+  # Configure the AWS provider
 provider "aws" {
-  region = "eu-north-1"
+  region = "eu-north-1"  # AWS region where resources will be created
 }
 
-resource "aws_vpc" "weather_app" {
-  cidr_block = "10.0.0.0/16"
+provider "archive" {
+  # Configuration options
 }
