212c6e30dc0887c28f2347dabe067f05fb84c1bb
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2024-09-14 07:34:35 +0200

    Application udate

--- backend/app.py
+++ backend/app.py
@@ -2,13 +2,13 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return jsonify({"message": "Welcome to the backend API!"})
-
-@app.route('/api/data')
+@app.route('/api/data', methods=['GET'])
 def get_data():
-    return jsonify({"data": "This is some data from the backend"})
+    return jsonify({
+        'message': 'Hello from the backend!',
+        'data': [1, 2, 3, 4, 5]
+    })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
