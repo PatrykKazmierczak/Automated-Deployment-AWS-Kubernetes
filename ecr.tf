8e146b1f507c833424e97f8bf73edbfcd9c5442d
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2024-09-16 09:55:43 +0200

    prepare ecr

--- ecr.tf
+++ ecr.tf
@@ -1,45 +1,8 @@
-# Define the ECR repository
-resource "aws_ecr_repository" "weather-app" {
-  name = "weather-app"
-}
+resource "aws_ecr_repository" "foo" {
+  name                 = "weather-app"
+  image_tag_mutability = "MUTABLE"
 
-# Create IAM policy document for ECR access
-data "aws_iam_policy_document" "weather-app" {
-  statement {
-    sid    = "AllowECRActions"  
-    effect = "Allow"
-
-    actions = [
-      "ecr:GetDownloadUrlForLayer",
-      "ecr:BatchGetImage",
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:PutImage",
-      "ecr:InitiateLayerUpload",
-      "ecr:UploadLayerPart",
-      "ecr:CompleteLayerUpload",
-      "ecr:DescribeRepositories",
-      "ecr:GetRepositoryPolicy",
-      "ecr:ListImages",
-      "ecr:DeleteRepository",
-      "ecr:BatchDeleteImage",
-      "ecr:SetRepositoryPolicy",
-      "ecr:DeleteRepositoryPolicy",
-      "ecr:DescribeRepositories"
-    ]
-
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::123456789012:role/ECRAccessRole"]  # Replace with a valid role/account
-    }
-
-    resources = [
-      aws_ecr_repository.weather-app.arn  # Reference the ECR repository's ARN dynamically
-    ]
+  image_scanning_configuration {
+    scan_on_push = true
   }
-}
-
-# Attach the policy to the ECR repository
-resource "aws_ecr_repository_policy" "weather-app" {
-  repository = aws_ecr_repository.weather-app.name
-  policy     = data.aws_iam_policy_document.weather-app.json
 }
